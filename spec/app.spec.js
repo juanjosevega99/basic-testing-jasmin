f3d6465b3232403495a333faa059427d355fadfc
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-03 12:27:42 -0500

    first set test with jasmine

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,5 +1,21 @@
 const greet = require("../app");
 
-it("The function greet ", () => {
-  expect(greet("Platzi")).toBe("Hi Platzi");
+// boolean, string, number
+var x = false;
+var a = {};
+var b = {};
+
+describe("check that the variable is true", () => {
+  it("The function greet ", () => {
+    expect(greet("Platzi")).toContain("Hi");
+  });
+
+  it("X is true", () => {
+    expect(x).toBe(false);
+    expect(x).toEqual(false);
+  });
+
+  it("Objects equals", () => {
+    expect(a).toEqual(b);
+  });
 });
